9eacdfddafae1d83139a429fb803116dc0e152d3
Eikyu42 <sammael0109@gmail.com>
2023-04-22 23:41:51 +0200

    finished the background of the trolley level

--- Godot/script/trolley.gd
+++ Godot/script/trolley.gd
@@ -9,6 +9,17 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	velocity.x = m_speed;
+	
 
 func _physics_process(delta):
 		move_and_slide();
+		
+		
+func _process(delta):
+	pass
+	
+func MoveUp():
+	pass
+	
+func MoveDown():
+	pass
