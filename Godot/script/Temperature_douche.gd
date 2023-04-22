99e949d0c22fb110b55d9bea37dfa3dc27aee714
Roulalou <sebastien.gios@student.uclouvain.be>
2023-04-22 19:52:31 +0200

    anime feu

--- Godot/script/Temperature_douche.gd
+++ Godot/script/Temperature_douche.gd
@@ -24,10 +24,13 @@ func _on_timer_timeout():
 	
 	# value not  between x and y : lose hp
 	if $TDouche.value < 700 || $TDouche.value >800:
+		$SebLaverChaud.visible = true
 		$HP.value -= 3
 		if $HP.value <= 0:
 			global_vars.hp -= 1
 			get_tree().change_scene_to_file("res://scene/transition.tscn")
+	else:
+		$SebLaverChaud.visible = false
 	
 func _input(event):
 	if event.is_action_pressed("doucheUP"):
