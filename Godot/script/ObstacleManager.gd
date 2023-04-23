35224cc9025d429bd532055bd3eea5456ce6d90a
Eikyu42 <sammael0109@gmail.com>
2023-04-23 02:06:34 +0200

    placed obstacles by hand

---
+++ Godot/script/ObstacleManager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var m_obstacle = preload("res://Assets/Prefabs/people.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SpawnObstacle()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func SpawnObstacle():
+	var instance = m_obstacle.instantiate()
+	var root = get_tree().get_root()
+	root.add_child(instance)
+	
