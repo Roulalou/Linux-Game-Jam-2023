cc4508f08591beafa6459d742ed58be20273581d
Eikyu42 <sammael0109@gmail.com>
2023-04-23 02:32:13 +0200

    added speed getting higher with levels

--- Godot/script/trolley.gd
+++ Godot/script/trolley.gd
@@ -3,12 +3,17 @@ extends CharacterBody2D
 
 @export var m_speed : float = 50.0;
 
+@export var m_speedDifficultyOffset: float = 20.0;
+
+@onready
+var global_vars = get_node("/root/Global")
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _ready():
-	velocity.x = m_speed;
+	velocity.x = m_speed + (m_speedDifficultyOffset * global_vars.level);
 	
 func _process(delta):
 	pass
@@ -31,3 +36,4 @@ func MoveUp():
 func MoveDown():
 	position.y += 122;
 	pass
+
